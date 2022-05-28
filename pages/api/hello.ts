5322786946593a4c70aa824608d2d896083075f3
kida-works <kida.works.1234@gmail.com>
2022-05-28 17:20:48 +0900

    add

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,119 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+export type DateType = [InnerDataTypes]
+
+interface InnerDataTypes  {
+  title: string,
+  lists: [ListsType]
 }
+
+
+interface ListsType {
+  text: string,
+  href: string
+}
+
+
+
+const Data:DateType =  [
+  {
+    "title": "HTML/CSS",
+    "lists": [
+      {
+      "text": "レスポンシブ対応時のスタイルの指定方法",
+      "href": "posts"
+      },
+      {
+        "text": "test",
+        "href": "posts"
+      },
+      {
+        "text": "レスポンシブ対応時のスタイルの指定方法",
+        "href": "posts"
+      },
+    ]
+  },
+  {
+    "title": "JavaScriptについて",
+    "lists":  [
+      {
+      "text": "レスポンシブ対応時のスタイルの指定方法",
+      "href": "posts"
+      },
+      {
+        "text": "test",
+        "href": "posts"
+      },
+      {
+        "text": "レスポンシブ対応時のスタイルの指定方法",
+        "href": "posts"
+      },
+    ]
+  },
+  {
+    "title": "React",
+    "lists":  [
+      {
+      "text": "レスポンシブ対応時のスタイルの指定方法",
+      "href": "posts"
+      },
+      {
+        "text": "test",
+        "href": "posts"
+      },
+      {
+        "text": "レスポンシブ対応時のスタイルの指定方法",
+        "href": "posts"
+      },
+    ]
+  },
+  {
+    "title": "Swift",
+    "lists":  [
+      {
+      "text": "レスポンシブ対応時のスタイルの指定方法",
+      "href": "posts"
+      },
+      {
+        "text": "test",
+        "href": "posts"
+      },
+      {
+        "text": "レスポンシブ対応時のスタイルの指定方法",
+        "href": "posts"
+      },
+    ]
+  },
+  {
+    "title": "WebAPI",
+    "lists":  [
+      {
+      "text": "レスポンシブ対応時のスタイルの指定方法",
+      "href": "posts"
+      },
+      {
+        "text": "test",
+        "href": "posts"
+      },
+      {
+        "text": "レスポンシブ対応時のスタイルの指定方法",
+        "href": "posts"
+      },
+    ]
+  },
+]
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<DataTypes>
+// ) {
+//   res.status(200).json(Data)
+// }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(Data)
 }
