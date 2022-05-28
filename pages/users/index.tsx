bfd377d94aa021f800a56f39f8f30ecbf376f308
kida-works <kida.works.1234@gmail.com>
2022-05-28 23:18:29 +0900

    add

--- pages/users/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-// import { Request, Response} from 'express';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users/')
-  const users = await response.json()
-  res.status(200).json({ users })
-}
