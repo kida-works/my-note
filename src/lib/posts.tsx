48ff83af6adf0d524a39e93340d8ff972f0b367e
kida-works <kida.works.1234@gmail.com>
2022-06-20 23:32:10 +0900

    add

--- src/lib/posts.tsx
+++ src/lib/posts.tsx
@@ -32,7 +32,7 @@ export function getSortedPostsData() {
     }
   })
   // Sort posts by date
-  return allPostsData.sort(({ date: a }:any, { date: b }:any) => {
+  return allPostsData.sort(({ date: a }: any, { date: b }: any) => {
     if (a < b) {
       return 1
     } else if (a > b) {
